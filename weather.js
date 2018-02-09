268f4ef0e51e29ca474390c149dd985ff8564494
kramduckner <buckner.a.mark@gmail.com>
2018-02-09 08:07:52 -0700

    Create weather.js

---
+++ weather.js
@@ -0,0 +1,11 @@
+var city = "Marrakech";
+
+var data = [
+  {temperatureFarenheit:22, preciptation: 0 , city: "Beijing" },
+  {temperatureFarenheit:56, precipitation: 10, city:"Murrakech" },  
+  {temperatureFarenheit:3, precipitation:84, city:"Bozeman"},
+]
+
+//using the data array, find and match the variable "city" to the proper element in "data." Use the match to build a string
+//that says, "The weather today is: temp: {temperature (in c)} with precipitation at : {precipitation}%" However, the temp 
+//that gets displayed needs to in celsius!
