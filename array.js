ed67ebcfb013961bffa4de85db80433312e4f659
kramduckner <buckner.a.mark@gmail.com>
2018-02-09 07:39:02 -0700

    Create array.js

---
+++ array.js
@@ -0,0 +1,4 @@
+//Build a javascript function that iterates over an array of strings and capitalizes the first letter of each string.
+//Use tdd to write this. 
+
+//To introduce you to "higher-order functions" utilize the array.forEach() method.
