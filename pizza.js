0ecf9c191cf125288c9b248214513c62a9f6145b
kramduckner <buckner.a.mark@gmail.com>
2018-02-09 07:35:01 -0700

    Create pizza.js

---
+++ pizza.js
@@ -0,0 +1,31 @@
+var toppings1 = {
+  pepperoni:9,
+  onions:0,
+  blackOlives:40,
+  sauce:"white",
+  crust:{
+    type:"stuffed",
+    contents:"cheese"
+  }
+}
+
+var toppings2 = {
+  pepperoni:0,
+  onions:99,
+  blackOlives:40,
+  sauce:"red",
+  crust:{
+    type:"crusty",
+    contents:""
+  }
+}
+
+var counter = 0;
+
+//write a function that returns "Dominays Pizza Order no: {counter}. It is a pizza with {pepperoni} pepperonis, {onions} onion, 
+//{blackOlives} blackOlives, {sauce} sauce, and a crust of type: {type} containing {contents}"
+//use the counter to increment the counter for each order number, so that each order has a unique order number.
+
+//finally, build one more object, toppings3, populated with the same keys as toppings1 and toppings2. Give the keys values and
+//pass it into your function.
+
